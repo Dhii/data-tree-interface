3bd33d1485b8a83606b830b8833c47a3277aec44
Anton Ukhanev <xedin.unknown@gmail.com>
2017-07-07 06:28:15 +0200

    Added `ParentAwareNodeInterface` and test

---
+++ src/ParentAwareNodeInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dhii\Data\Tree;
+
+use Dhii\Data\ParentAwareInterface;
+
+/**
+ * A node that can have its parent retrieved.
+ *
+ * @since [*next-version*]
+ */
+interface ParentAwareNodeInterface extends
+        NodeInterface,
+        ParentAwareInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     *
+     * @return BasicNodeInterface|null The node's parent, if any.
+     */
+    public function getParent();
+}
---
+++ test/functional/ParentAwareNodeInterfaceTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Dhii\Data\Tree\FuncTest;
+
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see \Dhii\Data\Tree\ParentAwareNodeInterface}.
+ *
+ * @since [*next-version*]
+ */
+class ParentAwareNodeInterfaceTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\\Data\\Tree\\ParentAwareNodeInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return \Dhii\Data\Tree\ParentAwareNodeInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->getValue()
+            ->getParent()
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME, $subject, 'A valid instance of the subject could not be created'
+        );
+        $this->assertInstanceOf(
+            'Dhii\\Data\\Tree\\NodeInterface', $subject, 'Subject does not extend required interface'
+        );
+    }
+}
