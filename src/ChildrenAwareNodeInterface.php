dc2a32c7bcb6fe8b9ca166a3bd7911607f971d33
Anton Ukhanev <xedin.unknown@gmail.com>
2017-07-07 06:26:23 +0200

    Added `ChildrenAwareNodeInterface` and test

---
+++ src/ChildrenAwareNodeInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Dhii\Data\Tree;
+
+use Dhii\Data\ChildrenAwareInterface;
+use Traversable;
+
+/**
+ * A node that can have its children retrieved.
+ *
+ * @since [*next-version*]
+ */
+interface ChildrenAwareNodeInterface extends
+    NodeInterface,
+    ChildrenAwareInterface
+{
+    /**
+     * Retrieves child nodes of this node.
+     *
+     * @since [*next-version*]
+     *
+     * @return BasicNodeInterface[]|Traversable A list of child nodes.
+     */
+    public function getChildren();
+}
